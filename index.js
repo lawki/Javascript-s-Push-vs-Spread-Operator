66f8b2e3ee87b9daf538eed37bec155b37b743ca
lawki <prakharpalpfdr+github@gmail.com>
2019-02-24 00:03:22 +0530

    use local version of chart.js for drawing charts

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+/**
+ * Author: Prakhar Pal(https://github.com/lawki)
+ * Usage: serves static assets and handles RESTful requests
+ */
+
 const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"node_modules","chart.js")));
 app.get("/",(req,res)=>{
 	res.sendFile(path.join(__dirname,'public','index.html'));
 });
